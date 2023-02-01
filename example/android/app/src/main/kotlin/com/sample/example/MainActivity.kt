274e7d10e93adcd3f58e6f4f46a86d34aec169da
Shreeman Arjun Sahu <shreemanarjunsahu@gmail.com>
2023-02-01 22:11:20 +0530

    fix: example

---
+++ example/android/app/src/main/kotlin/com/sample/example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sample.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/edge_detection.dart
+++ lib/edge_detection.dart
@@ -3,15 +3,15 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class EdgeDetection {
-  static const MethodChannel _channel = const MethodChannel('edge_detection');
+  static const MethodChannel _channel = MethodChannel('edge_detection');
 
   /// Call this method to scan the object edge in live camera.
   static Future<bool> detectEdge(String saveTo,
-      {bool canUseGallery: true,
-      String androidScanTitle: "Scanning",
-      String androidCropTitle: "Crop",
-      String androidCropBlackWhiteTitle: "Black White",
-      String androidCropReset: "Reset"}) async {
+      {bool canUseGallery = true,
+      String androidScanTitle = "Scanning",
+      String androidCropTitle = "Crop",
+      String androidCropBlackWhiteTitle = "Black White",
+      String androidCropReset = "Reset"}) async {
     return await _channel.invokeMethod('edge_detect', {
       'save_to': saveTo,
       'can_use_gallery': canUseGallery,
@@ -24,9 +24,9 @@ class EdgeDetection {
 
   /// Call this method to scan the object edge from a gallery image.
   static Future<bool> detectEdgeFromGallery(String saveTo,
-      {String androidCropTitle: "Crop",
-      String androidCropBlackWhiteTitle: "Black White",
-      String androidCropReset: "Reset"}) async {
+      {String androidCropTitle = "Crop",
+      String androidCropBlackWhiteTitle = "Black White",
+      String androidCropReset = "Reset"}) async {
     return await _channel.invokeMethod('edge_detect_gallery', {
       'save_to': saveTo,
       'crop_title': androidCropTitle,
